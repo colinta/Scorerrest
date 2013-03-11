4ba958f2faae1c03529cc662944317f437b00bab
Colin T.A. Gray <colinta@gmail.com>
2013-03-11 09:59:45 -0600

    combine app_launcher stuff into one file

--- lib/applauncher_view.rb
+++ lib/applauncher_view.rb
@@ -1,4 +1,3 @@
-
 class AppLauncherView < UIControl
 
   def initialize(name, icon)
@@ -43,6 +42,40 @@ class AppLauncherView < UIControl
 
   def intrinsicContentSize
     @icon.size + CGSize.new(26, 30)
+  end
+
+end
+
+
+Teacup::Stylesheet.new(:app_launcher) do
+
+  if device_is? iPhone
+    style :app_launcher,
+      width: 80,
+      height: 84
+  else
+    style :app_launcher,
+      width: 98,
+      height: 102
+  end
+
+  style :icon, constraints: [
+      :center_x, constrain_top(10)
+    ]
+
+  style :title, constraints: [
+      :center_x, constrain_below(:icon, 2)
+    ],
+    textAlignment: :center.uialignment,
+    textColor: :white.uicolor,
+    backgroundColor: :clear.uicolor
+
+  if device_is? iPhone
+    style :title,
+      font: :bold.uifont(10)
+  else
+    style :title,
+      font: :bold.uifont(14)
   end
 
 end
